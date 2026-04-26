4dccde7b73caa06e0eb2046b1ead6aa62b58cb9d
AP24110010769 <banavathuramkumar@gmail.com>
2026-04-26 10:12:46 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://fooddelivery:fooddelivery123@cluster0.2xond9r.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://fooddelivery:fooddelivery@cluster0.2xond9r.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
