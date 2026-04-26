da564aae8c3c3c19417796910a0390458b5a56a0
BANAVATHU RAMKUMAR <banavathuramkumar@gmail.com>
2026-04-26 11:44:22 +0530

    Update backend URL in StoreContext

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://food-delivery-backend-ps22.onrender.com"
+    const url = "https://food-del-backend-yo3h.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
